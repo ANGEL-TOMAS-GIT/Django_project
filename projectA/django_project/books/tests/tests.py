752fbc983961d492142e2555ca1b9bff77e3b7ca
angel-tomas <angelotomas.atgr@gamil.com>
2026-05-28 23:03:14 +0200

    Adding Project B to gitHub Action

--- projectA/django_project/books/tests/tests.py
+++ projectA/django_project/books/tests/tests.py
@@ -4,8 +4,8 @@ from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
 from books.models import Book
+from custom_user_account.models import CustomUser as User
 
-User = get_user_model()
 
 class BookModelTest(TestCase):
     def setUp(self):
