51cb501eac595af15617afea8fbfc8b925c5bf6d
angel-tomas <angelotomas.atgr@gamil.com>
2026-05-30 23:51:13 +0200

    Adding Project B to gitHub Action

--- projectA/django_project/books/tests/test_views.py
+++ projectA/django_project/books/tests/test_views.py
@@ -6,7 +6,7 @@ from .factories import BookFactory
 @pytest.mark.django_db
 def test_books_list(client):
     response = client.get(reverse("books"), follow=True)
-
+    
     assert response.status_code == 200
 
 
@@ -16,19 +16,19 @@ def test_book_detail(client):
         title="Harry Potter",
         stock=2
     )
-
+    
     response = client.get(
         reverse("book_detail", args=[book.pk]),
         follow=True
     )
-
+    
     assert response.status_code == 200
 
 
 @pytest.mark.django_db
 def test_book_str():
     book = BookFactory(title="Harry Potter")
-
+    
     assert "Harry Potter" in str(book)
 
 
@@ -55,14 +55,28 @@ def test_book_detail_404(client):
     response = client.get(reverse("book_detail", args=[99999]), follow=True)
     assert response.status_code == 404
 
+
 def test_book_update_view_requires_login(client):
     response = client.get(reverse('update_book', args=[1]))
     assert response.status_code in [301, 302]
+
 
 def test_book_delete_view_requires_login(client):
     response = client.get(reverse('delete_book', args=[1]))
     assert response.status_code in [301, 302]
 
+
 def test_cart_view_requires_login(client):
     response = client.get(reverse('cart_detail'))
     assert response.status_code in [301, 302]
+
+
+@pytest.mark.django_db
+def test_book_search_filter(client):
+    response = client.get(reverse('books'), {'search': 'test'}, follow=True)
+    assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_book_price_filter(client):
+    response = client.get(reverse('books'), {'min_price': 10, 'max_price': 50}, follow=True)
+    assert response.status_code == 200
